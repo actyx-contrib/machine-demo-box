62b7a83c1302075919dd875d6d1250dc0152f1e3
Wolfgang Werner <mail@wolfgang-werner.net>
2021-04-01 14:22:09 +0200

    Allow for more variants in test data

--- src/worker-ui/App.tsx
+++ src/worker-ui/App.tsx
@@ -1,13 +1,21 @@
 import { usePond } from '@actyx-contrib/react-pond'
 import * as React from 'react'
 import { ErrorFish } from '../fish/ErrorFish'
-import { Button } from './components/Button'
 import { ErrorList } from './components/ErrorList'
 import { Header } from './components/Header'
 import * as uuid from 'uuid'
+import { ToggleButtons } from '@actyx/industrial-ui'
+import { Button } from './components/Button'
 
 export const App = (): JSX.Element => {
   const [devMode, setDevMode] = React.useState(false)
+  const [devModeErrorType, setDevModeErrorType] = React.useState('Error A')
+  const [devModeErrorMachine, setDevModeErrorMachine] = React.useState('m1')
+
+  const devModeMockError = () => {
+    ErrorFish.emitErrorOccurredEvent(pond, uuid.v1(), devModeErrorMachine, 10, devModeErrorType)
+  }
+
   const pond = usePond()
   React.useEffect(() => {
     document.addEventListener('keydown', (e) => {
@@ -21,12 +29,41 @@ export const App = (): JSX.Element => {
     <div>
       <Header text="Machine Error Overview" />
       {devMode && (
-        <Button
-          variant="flat"
-          color="primary"
-          onClick={() => ErrorFish.emitErrorOccurredEvent(pond, uuid.v1(), 'm1', 10, 'Error A')}
-          text="send event"
-        />
+        <div style={{ display: "flex" }}>
+
+          <div style={{ margin: "1em" }}>
+            <ToggleButtons
+              onToggle={id => setDevModeErrorMachine(id)}
+              items={[
+                { id: 'm1', label: 'Machine 1' },
+                { id: 'm2', label: 'Machine 2' }
+              ]}
+              initToggledItemId="third"
+            />
+          </div>
+
+          <div style={{ margin: "1em" }}>
+            <ToggleButtons
+              onToggle={id => setDevModeErrorType(id)}
+              items={[
+                { id: 'Error A', label: 'Error A' },
+                { id: 'Error B', label: 'Error B' }
+              ]}
+              initToggledItemId="third"
+            />
+          </div>
+
+          <div style={{ margin: "1em" }}>
+            <Button
+              variant="raised"
+              color="primary"
+              onClick={devModeMockError}
+              text="send"
+            />
+          </div>
+
+
+        </div>
       )}
       <div style={{ padding: '24px 24px' }}>
         <ErrorList />
