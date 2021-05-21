2b7b4f3e49dc38367804a1193ecf1140466ca8aa
Alex Halemba <alex-halemba@gmx.de>
2021-05-21 16:45:57 +0200

    emit value events

---
+++ src/opcua-connector/values.ts
@@ -0,0 +1,36 @@
+import { distinctUntilChanged, map, tap } from 'rxjs/operators'
+import { OpcuaStreams, Value, Values } from './types'
+
+const toValue = (settings: Value) => (value: unknown): number => {
+  let nr = typeof value === 'string' || typeof value === 'number' ? +value : 0
+
+  if (settings.decimal !== undefined || settings.scale !== undefined) {
+    if (settings.scale !== undefined) {
+      nr *= settings.scale
+    }
+    if (settings.decimal !== undefined) {
+      nr = +nr.toFixed(settings.decimal)
+    }
+  }
+
+  return nr
+}
+
+export const executeValueEmitter = (
+  streams: OpcuaStreams,
+  values: Values,
+  emit: (name: string, value: number) => void,
+): void =>
+  Object.entries(values).forEach(([name, settings]) => {
+    if (streams.hasOwnProperty(name)) {
+      const key = name as keyof OpcuaStreams
+      streams[key]
+        .pipe(
+          map((v) => v.value),
+          distinctUntilChanged((prev, cur) => !settings.distinct || prev !== cur),
+          map(toValue(settings)),
+          tap((v) => emit(name, v)),
+        )
+        .subscribe()
+    }
+  })
