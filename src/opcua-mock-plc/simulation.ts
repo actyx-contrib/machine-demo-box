b5a7e5d87ea5ac884c92f67a2054322715af519e
Alex Halemba <alex-halemba@gmx.de>
2021-05-21 11:27:05 +0200

    auth for opcua Mock PLC

--- src/opcua-mock-plc/simulation.ts
+++ src/opcua-mock-plc/simulation.ts
@@ -6,53 +6,60 @@ enum State {
   ERROR,
 }
 
-export const simulateMachine = (device: UAObject, namespace: Namespace): void => {
+export const simulateMachine = (device: UAObject, namespace: Namespace): (() => void) => {
   // PLC state
   let state = State.IDLE
   let speed = 0
   let temp = 20
 
+  const inters: NodeJS.Timeout[] = []
   // simulate PLC behavior
-  setInterval(() => {
-    temp += (Math.random() - (state === State.RUNNING ? 0.5 : 1.0)) * 0.1
-    temp = Math.max(25, Math.min(55, temp))
-    temp = Math.floor(temp * 1000) / 1000
-  }, 2_500)
+  inters.push(
+    setInterval(() => {
+      temp += (Math.random() - (state === State.RUNNING ? 0.5 : 1.0)) * 0.1
+      temp = Math.max(25, Math.min(55, temp))
+      temp = Math.floor(temp * 1000) / 1000
+    }, 2_500),
+  )
 
-  setInterval(() => {
-    if (state === State.RUNNING) {
-      speed += (Math.random() - 0.5) * 0.1
-      speed = Math.max(0.2, Math.min(1.8, speed))
-      speed = Math.floor(speed * 1000) / 1000
-    } else if (speed > 0) {
-      speed = 0
-    }
-  }, 4_000)
+  inters.push(
+    setInterval(() => {
+      if (state === State.RUNNING) {
+        speed += (Math.random() - 0.5) * 0.1
+        speed = Math.max(0.2, Math.min(1.8, speed))
+        speed = Math.floor(speed * 1000) / 1000
+      } else if (speed > 0) {
+        speed = 0
+      }
+    }, 4_000),
+  )
 
-  setInterval(() => {
-    const lastState = state
-    switch (state) {
-      case State.IDLE: {
-        if (Math.random() > 0.25) {
-          state = Math.random() > 0.95 ? State.ERROR : State.RUNNING
+  inters.push(
+    setInterval(() => {
+      const lastState = state
+      switch (state) {
+        case State.IDLE: {
+          if (Math.random() > 0.25) {
+            state = Math.random() > 0.95 ? State.ERROR : State.RUNNING
+          }
+          break
         }
-        break
-      }
-      case State.RUNNING: {
-        if (Math.random() > 0.93) {
-          state = Math.random() > 0.6 ? State.ERROR : State.IDLE
+        case State.RUNNING: {
+          if (Math.random() > 0.93) {
+            state = Math.random() > 0.6 ? State.ERROR : State.IDLE
+          }
+          break
         }
-        break
-      }
-      case State.ERROR: {
-        if (Math.random() > 0.66) {
-          state = Math.random() > 0.8 ? State.RUNNING : State.IDLE
+        case State.ERROR: {
+          if (Math.random() > 0.66) {
+            state = Math.random() > 0.8 ? State.RUNNING : State.IDLE
+          }
+          break
         }
-        break
       }
-    }
-    console.log('change state from: ', lastState, state)
-  }, 10_000)
+      console.log('change state from: ', lastState, state)
+    }, 10_000),
+  )
 
   namespace.addVariable({
     componentOf: device,
@@ -87,4 +94,6 @@ export const simulateMachine = (device: UAObject, namespace: Namespace): void =>
       },
     },
   })
+
+  return () => inters.forEach(clearInterval)
 }
